c8abefee92cb4335ea5f56984b7a23d612264252
Hopper Gee <hopper.gee@hey.com>
2024-08-07 22:44:14 +0800

    Build a minify version also

--- esbuild.mjs
+++ esbuild.mjs
@@ -31,3 +31,34 @@ esbuild.build({
     WORKER_FILE_CONTENT: JSON.stringify(workerFileContent)
   }
 });
+
+// Build heic-to.min.js
+
+await esbuild.build({
+  entryPoints: [
+    'src/worker.js'
+  ],
+  bundle: true,
+  minify: true,
+  target: 'es6',
+  platform: 'browser',
+  outfile: 'tmp/worker.min.js',
+  external: ['fs', 'path']
+});
+
+const workerFileMinifyContent = fs.readFileSync('tmp/worker.min.js', 'utf8')
+
+esbuild.build({
+  entryPoints: [
+    'src/index.js'
+  ],
+  bundle: true,
+  minify: true,
+  target: 'es6',
+  platform: 'browser',
+  outfile: 'dist/heic-to.min.js',
+  format: 'esm',
+  define: {
+    WORKER_FILE_CONTENT: JSON.stringify(workerFileMinifyContent)
+  }
+});
